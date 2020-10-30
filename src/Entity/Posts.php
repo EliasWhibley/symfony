bf9b80ab123d684d85b1b16a59d33851986e7918
Elías Huerta <elias.huerta@icloud.com>
2020-10-31 00:56:16 +0100

    Posts routes and API

---
+++ src/Entity/Posts.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PostsRepository::class)
+ */
+class Posts
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $create_date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreateDate(): ?\DateTimeInterface
+    {
+        return $this->create_date;
+    }
+
+    public function setCreateDate(?\DateTimeInterface $create_date): self
+    {
+        $this->create_date = $create_date;
+
+        return $this;
+    }
+
+
+
+    /**
+     * @throws \Exception
+     * @ORM\PrePersist()
+     */
+    public function beforeSave()
+    {
+
+        $this->create_date = new \DateTime('now', new \DateTimeZone('Africa/Casablanca'));
+    }
+
+
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return [
+            "name" => $this->getName(),
+            "description" => $this->getDescription()
+        ];
+    }
+}
